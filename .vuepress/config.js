ab2b85f8fa5c58068dcf50fc2847307ded3013d4
tiziodcaio <tiziodcaio@gmail.com>
2021-05-04 09:25:53 +0200

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -40,7 +40,6 @@ module.exports = {
         editLinks: true,
         smoothScroll: true,
         editLinkText: 'Aiutaci a tradurre questa pagina!',
-        logo: '.vuepress/public/homepage.png',
         nav: [
             {
                 text: 'Dortania Guides',
